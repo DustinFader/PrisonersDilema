202d788ae9cf4b1e4eb777adf055556d7b062a6d
Dustern <140115413+DustinFader@users.noreply.github.com>
2024-01-13 17:21:24 -0400

    Add files via upload

---
+++ PrisonersDilema/Prisoners Dilemma.md
@@ -0,0 +1,35 @@
+Notes:
+How about trees of dilemmas? Or other variations of complexity? Instead of specified or uniform groups of dilemmas in focus.
+
+TitforTat works as the best according to the video but might not work well when what is seen may not always be what you think. Hence a bit of forgiveness is nessacery and makes it work better. Tho in real life its not good to have too much forgiveness so future tests with defection and forgiveness. 
+
+- Cooperate
+- Defection
+- Forgiveness
+- Vengeance/Retaliatory
+
+References: [video](https://www.youtube.com/watch?v=mScpHTIi-kM)
+
+Possible outcomes per game
+   33
+50, 05
+   11
+
+Planned-features: 
+- Mixed Groups of players against other groups of players where the games aren't always with the same players.
+
+- Permutations of players with names set to some. Like tit for tat.
+Non hard coded. But so far is.
+
+- Color the percent as green if it has more and red if less.
+
+- input the strategy as the name instead of player.
+
+- Put strategies into seperate file.
+
+Current-features: 
+- Random or not random of false perspectives of colluding or defects.
+Maybe separate values of whether they mistake defects and colludes.
+AKA those who are more prone to be pessimistic maybe?
+
+End result is a percentage where the highest is the one with the most score. 
---
+++ PrisonersDilema/PrisonersDilema.js
@@ -0,0 +1,110 @@
+class Player {
+    constructor() {
+        this.history = [];
+    }
+}
+
+class TitForTat extends Player {
+    alg(vs) {
+        return vs.history.length ? vs.history[0] : 1;
+        // Player colludes as default
+        // Player defects only if last game the other defects
+    };
+};
+
+class TatForTit extends Player {
+    alg(vs) {
+        return vs.history.length ? 1-vs.history[0] : 1;
+        // Player colludes as default
+        // Player does the opposite of the other player
+    };
+};
+
+class Defector extends Player {
+    alg(vs) {
+        // always defect
+        return 0;
+    };
+};
+
+class Colluder extends Player {
+    alg(vs) {
+        // always cooperate
+        return 1;
+    };
+};
+
+class Tester extends Player {
+    alg(vs) {
+        // defect at first and if no vengeance then defect every other time. 
+        // Else titfortat afterward.
+        // not correct as of yet
+        if (!vs.history.length) {
+            return 0;
+        }
+
+        if (vs.history.includes(0)) {
+            return vs.history[0];
+        }
+        return 0;
+    };
+};
+
+
+// choice
+// cooperate 1
+// defect 0
+const games = (player1Act, player2Act) => {
+    let score = [0, 0];
+    let gameLength = 100;
+    let error = 0.1; // i could make it non universale
+
+    for (let i = 0; i < gameLength; i++) {
+        // Reverse true to false and vice versa with a percentage of error flipping it.
+        let player1Seen = player1Act.alg(player2Act)
+        let player2Seen = player2Act.alg(player1Act)
+        if (Math.random() < error) {
+            player1Seen = !player1Seen;
+            player2Seen = !player2Seen;
+        }
+
+        // both cooperate
+        if (player1Seen && player2Seen) {
+            score[0] += 3;
+            score[1] += 3;
+            player1Act.history.unshift(1);
+            player2Act.history.unshift(1);
+            continue;
+        }
+        // Player1 cooperate
+        if (player1Seen && !player2Seen) {
+            score[0] += 0;
+            score[1] += 5;
+            player1Act.history.unshift(1);
+            player2Act.history.unshift(0);
+            continue;
+        }
+        // player2 cooperate
+        if (!player1Seen && player2Seen) {
+            score[0] += 5;
+            score[1] += 0;
+            player1Act.history.unshift(0);
+            player2Act.history.unshift(1);
+            continue;
+        }
+        // none cooperate
+        if (!player1Seen && !player2Seen) {
+            score[0] += 1;
+            score[1] += 1;
+            player1Act.history.unshift(0);
+            player2Act.history.unshift(0);
+        }
+    }
+    console.log("p1", player1Act.history, "p2", player2Act.history);
+    return `PLayer 1: ${Math.round(score[0] / (score[0] + score[1]) * 100)}% Player 2: ${Math.round(score[1] / (score[1] + score[0]) * 100)}%`;
+}
+
+let player1 = new TitForTat();
+let player2 = new TitForTat();
+
+console.log(games(player1, player2));
